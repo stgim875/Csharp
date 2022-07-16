f062468c4fae0c18902ec17d3243768cf4265664
Gim <stgim875@gmail.com>
2022-07-16 15:49:19 +0900

    첫 수업 업데이트

---
+++ CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //데이터+로직
+            //체력 0
+
+            //byte(1바이트), short(2바이트), int(4바이트), long(8바이트)
+            //sbyte, ushort, unit, ulong
+            int hp;
+
+            hp = 100;
+
+            Console.WriteLine("Hello Numbr{0}", hp);
+        }
+    }
+}
